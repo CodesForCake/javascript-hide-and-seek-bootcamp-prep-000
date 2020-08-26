d741a3346a18ae38f3d023d85851387acdc97dc4
Mimi C <mina123fire@yahoo.com>
2020-08-26 03:48:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  var rankList = document.querySelectorAll(".ranked-list");
+  for (let i=0;i<rankList.length;i++){
+    var rankListChildren = rankList[i].children;
+    for (let j=0;j<rankListChildren.length;j++) {
+      rankListChildren[j].innerHTML=parseInt(rankListChildren[j])+parseInt(n);
+    }
+  }
+}
+function deepestChild(node){
+/*  var firstDiv = document.getElementById('grand-node').querySelector('div');
+  if (firstDiv.innerHTML === "boo!"){
+    return firstDiv.innerHTML;
+  } else {
+    deepestChild(firstDiv);  
+  }*/
+}
