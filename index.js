f05163d637cd0c9b556c23355c1ea04734edf2df
Mimi C <mina123fire@yahoo.com>
2020-08-26 04:07:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  var rankList = document.querySelectorAll(".ranked-list li");
+  for (let i=0;i<rankList.length;i++){
+    rankList[i].innerHTML=parseInt(rankList[i].innerHTML)+n;
+  }
+}
+
+function deepestChild(){
+  var parentDiv = document.querySelector('div#grand-node');
+  while (parentDiv.querySelector('div')) {
+    parentDiv=parentDiv.querySelector('div');
+  }
+  return parentDiv;
+}
