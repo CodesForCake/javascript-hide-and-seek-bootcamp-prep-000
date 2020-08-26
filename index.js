0455eddfa7847846aced538e3baf4e48abedbf8a
Mimi C <mina123fire@yahoo.com>
2020-08-26 04:05:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget(){
 function increaseRankBy(n) {
   var rankList = document.querySelectorAll(".ranked-list li");
   for (let i=0;i<rankList.length;i++){
-    rankList[i].innerHTML+=parseInt(rankList[i].innerHTML)+n;
+    rankList[i].innerHTML=parseInt(rankList[i].innerHTML)+n;
   }
 }
 
